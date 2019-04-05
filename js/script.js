63180890f19d7639b9a91184e230ac26d04e5e21
Mitchell Nelson <nels4929@stthomas.edu>
2019-04-05 13:35:55 -0500

    Initial Commit

---
+++ js/script.js
@@ -0,0 +1,11 @@
+init = function(){
+    
+            var mymap = L.map('mapid').setView([51.505, -0.09], 30);
+
+L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox.streets',
+    accessToken: 'pk.eyJ1IjoibmVsczQ5MjkiLCJhIjoiY2p1NGVqYjN3MHg0ejRkcnYzajRzOWhzYSJ9.EzP1fdcqxGe4aVr2_NqK1w'
+}).addTo(mymap);
+}
