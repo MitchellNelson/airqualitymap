f3d66f0b26f2206c40d9f8e265aabb6b18300cd4
Mitchell Nelson <nels4929@stthomas.edu>
2019-04-07 12:04:29 -0500

    Two Independent Maps implemented

--- js/script.js
+++ js/script.js
@@ -1,83 +1,74 @@
-/*var app;
-
-init = function(){
- 
-    
-    app = new Vue({
-        el: '#app',
-        data:{
-            lat:51.5,
-            long: -0.09
-        },
-        computed: {
-            center: function () {
-                return this.lat
-            }
-        },
-        watch: {
-          center: function(){
-            console.log('changed');
-          }
-        }
-    });
-    initMap();
-}
-
-initMap = function(){
-    var map = L.map('mapid').setView([51.505, -0.09], 30);
-    var marker = addMarker(51.5, -0.09, map);
-
-    L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
-        attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-        maxZoom: 18,
-        id: 'mapbox.streets',
-        accessToken: 'pk.eyJ1IjoibmVsczQ5MjkiLCJhIjoiY2p1NGVqYjN3MHg0ejRkcnYzajRzOWhzYSJ9.EzP1fdcqxGe4aVr2_NqK1w'
-    }).addTo(map);       
-    
-}
-
-addMarker = function(lat,long, map){
-    return L.marker([lat, long]).addTo(map);
-}
-*/
-
-
-
 var app;
-var starting_center = [44.9430, -93.1895];
+var starting_center1 = L.latLng(44.9430, -93.1895);
+var starting_center2 = L.latLng(40.7590, -73.9845);
 
 init = function(){
- 
-    
     app = new Vue({
         el: '#app',
         data:{
-            map: null,
-            center:starting_center
-        },
-        async mounted() {
-            this.initMap()
-            trackMap(map)
+            map1:null,
+            map2:null,
+            center1:starting_center1,
+            center2:starting_center2
         },
         methods: {
             initMap(){
-                map = L.map('mapid').setView(starting_center, 10);
+                this.map1 = L.map('map1id').setView(starting_center1, 13);
                 L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
                     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-                    maxZoom: 10,
+                    maxZoom: 30,
                     id: 'mapbox.streets',
                     accessToken: 'pk.eyJ1IjoibmVsczQ5MjkiLCJhIjoiY2p1NGVqYjN3MHg0ejRkcnYzajRzOWhzYSJ9.EzP1fdcqxGe4aVr2_NqK1w'
-                }).addTo(map);       
+                }).addTo(this.map1);      
+
+                this.map2 = L.map('map2id').setView(starting_center2, 13);
+                L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+                    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+                    maxZoom: 30,
+                    id: 'mapbox.streets',
+                    accessToken: 'pk.eyJ1IjoibmVsczQ5MjkiLCJhIjoiY2p1NGVqYjN3MHg0ejRkcnYzajRzOWhzYSJ9.EzP1fdcqxGe4aVr2_NqK1w'
+                }).addTo(this.map2);   
             },  
         }
     });
+    app.initMap();
+    trackMap();
 }
-trackMap = function(map){
-    map.addEventListener("move",function(){
-        app.center=map.getCenter();
+
+trackMap = function(){
+    app.map1.addEventListener("move",function(){
+        setTimeout(function(){ 
+            app.center1=app.map1.getCenter();
+        }, 200);
+    });
+    app.map2.addEventListener("move",function(){
+        setTimeout(function(){ 
+            app.center2=app.map2.getCenter();
+        }, 200);
     });
 }
 
-
-
-
+function latSearch1(event)
+{
+    setTimeout(function(){ 
+            app.map1.panTo(app.center1);
+        }, 600);
+}
+function lngSearch1(event)
+{
+    setTimeout(function(){ 
+            app.map1.panTo(app.center1);
+        }, 600);
+}
+function latSearch2(event)
+{
+    setTimeout(function(){ 
+            app.map2.panTo(app.center2);
+        }, 600);
+}
+function lngSearch2(event)
+{
+    setTimeout(function(){ 
+            app.map2.panTo(app.center2);
+        }, 600);
+}
