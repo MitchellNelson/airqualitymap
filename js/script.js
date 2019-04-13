61b3cd2e1ef442730631606850ef68719bc09d30
Mitchell Nelson <nels4929@stthomas.edu>
2019-04-13 11:32:22 -0500

    Both Maps Working with Location Input

--- js/script.js
+++ js/script.js
@@ -14,7 +14,9 @@ init = function(){
             map2:null,
             center1:starting_center1,
             center2:starting_center2,
-            unique_markers:[],
+            unique_markers1:[],
+            unique_markers2:[],
+            map1_marker_objects:[],
             map2_marker_objects:[],
             location1: starting_location1,
             location2: starting_location2
@@ -25,7 +27,8 @@ init = function(){
                 this.map1 = L.map('map1id').setView(starting_center1, 13);
                 L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
                     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-                    maxZoom: 30,
+                    maxZoom: 16,
+                    minZoom: 9,
                     id: 'mapbox.streets',
                     accessToken: 'pk.eyJ1IjoibmVsczQ5MjkiLCJhIjoiY2p1NGVqYjN3MHg0ejRkcnYzajRzOWhzYSJ9.EzP1fdcqxGe4aVr2_NqK1w'
                 }).addTo(this.map1);      
@@ -48,7 +51,7 @@ init = function(){
                     self.center1.lng = response.data[0].lon;
                 })
                 console.log(this.center1);
-                //this.map1.panTo(this.center1);
+                this.map1.panTo(this.center1);
             },
             mounted2() {
                 const self = this;
@@ -65,9 +68,26 @@ init = function(){
     });
     app.initMap();
     trackMap();
-    OpenAQSearch();
+    OpenAQSearch1();
+    OpenAQSearch2();
 }
-function OpenAQSearch(){
+function OpenAQSearch1(){
+    console.log("sending request");
+    //setup all vars to be plugged into the request url
+    var d = new Date();
+    var date_to = d.getFullYear()+"-"+(d.getMonth()+1)+"-"+d.getDate();
+    d.setDate(d.getDate() - 30);
+    var date_from = (d).getFullYear()+"-"+(d.getMonth()+1)+"-"+d.getDate();
+    var bounds = app.map1.getBounds();
+    var radius = app.map1.distance([bounds._northEast.lat,bounds._northEast.lng],[bounds._southWest.lat, bounds._southWest.lng])/2;
+    var request = {
+        url: "https://api.openaq.org/v1/measurements?order_by=location?date_from="+date_from+"&date_to="+date_to+"&coordinates="+app.center1.lat+","+app.center1.lng+"&radius="+radius+"&limit=10000",
+        dataType: "json",
+        success: FillUniqueMarkers1
+    };
+    $.ajax(request);
+}
+function OpenAQSearch2(){
     console.log("sending request");
     //setup all vars to be plugged into the request url
     var d = new Date();
@@ -79,12 +99,21 @@ function OpenAQSearch(){
     var request = {
         url: "https://api.openaq.org/v1/measurements?order_by=location?date_from="+date_from+"&date_to="+date_to+"&coordinates="+app.center2.lat+","+app.center2.lng+"&radius="+radius+"&limit=10000",
         dataType: "json",
-        success: FillUniqueMarkers
+        success: FillUniqueMarkers2
     };
-    console.log(request.url);
     $.ajax(request);
 }
-LocationFromLatLng = function(lat, lng, location) {
+LocationFromLatLng1 = function(lat, lng) {
+    const self = this;
+    axios
+    .get('https://nominatim.openstreetmap.org/reverse?format=json&lat='+lat+'&lon='+lng)
+    .then(response => {
+        if(response.data.address.city!=undefined){
+            app.location1 = response.data.address.city;
+        }
+    })
+}
+LocationFromLatLng2 = function(lat, lng) {
     const self = this;
     axios
     .get('https://nominatim.openstreetmap.org/reverse?format=json&lat='+lat+'&lon='+lng)
@@ -100,6 +129,30 @@ trackMap = function(){
             app.center1=app.map1.getCenter();
         }, 200);
     });
+    app.map1.addEventListener("dragend",function(){
+        if (openAQRequest!=null){
+            clearTimeout(openAQRequest);
+        }
+        openAQRequest = setTimeout(function(){ 
+            //DeleteOldMarkers();
+            OpenAQSearch1();
+            LocationFromLatLng1(app.center1.lat, app.center1.lng);
+            openAQRequest=null;
+        }, 200);
+    });
+    app.map1.addEventListener("zoomend",function(){
+        if (openAQRequest!=null){
+            clearTimeout(openAQRequest);
+        }
+        console.log("zoomend");
+        openAQRequest = setTimeout(function(){ 
+            //DeleteOldMarkers();
+            OpenAQSearch1();
+            LocationFromLatLng1(app.center1.lat, app.center1.lng);
+            openAQRequest=null;
+            console.log("in timeout")
+        }, 200);
+    });
     app.map2.addEventListener("move",function(){
         setTimeout(function(){ 
             app.center2=app.map2.getCenter();
@@ -112,8 +165,8 @@ trackMap = function(){
         }
         openAQRequest = setTimeout(function(){ 
             DeleteOldMarkers();
-            OpenAQSearch();
-            LocationFromLatLng(app.center2.lat, app.center2.lng);
+            OpenAQSearch2();
+            LocationFromLatLng2(app.center2.lat, app.center2.lng);
             openAQRequest=null;
         }, 200);
     });
@@ -124,8 +177,8 @@ trackMap = function(){
         console.log("zoomend");
         openAQRequest = setTimeout(function(){ 
             DeleteOldMarkers();
-            OpenAQSearch();
-            LocationFromLatLng(app.center2.lat, app.center2.lng, app.location2);
+            OpenAQSearch2();
+            LocationFromLatLng2(app.center2.lat, app.center2.lng);
             openAQRequest=null;
             console.log("in timeout")
         }, 200);
@@ -156,56 +209,106 @@ function lngSearch2(event){
 function locSearch1(event){
     setTimeout(function(){
            app.mounted();  
+           OpenAQSearch1();
     },1000);
 }
 
 function locSearch2(event){
     setTimeout(function(){
         console.log("going to send a request")
-        app.mounted2();  
-        //app.map2.panTo(app.center2); 
-        //locationRequest= null;
+        app.mounted2(); 
+        OpenAQSearch2(); 
     },1000);
 }
-function FillUniqueMarkers(data){
+function FillUniqueMarkers1(data){
     console.log(data.results);
     var num_new_markers=0;
     //var unique_markers = [];
     for(var i=0; i < data.results.length; i++){
         
         //if there is no markers in the array, add the first
-        if(app.unique_markers.length==0){
+        if(app.unique_markers1.length==0){
             var newmarker = new unique_marker(data.results[i].coordinates.latitude, data.results[i].coordinates.longitude);
             newmarker.addParameter(data.results[i].parameter,data.results[i].value);
-            app.unique_markers.push(newmarker);
+            app.unique_markers1.push(newmarker);
             num_new_markers++;
         }
 
         //check that if the new marker already exists in the unique_markers
         var exists = false;
-        for(var j = 0; j < app.unique_markers.length; j++) {
-            if (app.unique_markers[j].coordinates.latitude == data.results[i].coordinates.latitude && app.unique_markers[j].coordinates.longitude == data.results[i].coordinates.longitude){
+        for(var j = 0; j < app.unique_markers1.length; j++) {
+            if (app.unique_markers1[j].coordinates.latitude == data.results[i].coordinates.latitude && app.unique_markers1[j].coordinates.longitude == data.results[i].coordinates.longitude){
                 exists = true;
-                app.unique_markers[j].addParameter(data.results[i].parameter,data.results[i].value);
+                app.unique_markers1[j].addParameter(data.results[i].parameter,data.results[i].value);
             }
         }
         if(!exists){
             var newmarker = new unique_marker(data.results[i].coordinates.latitude, data.results[i].coordinates.longitude);
             newmarker.addParameter(data.results[i].parameter,data.results[i].value);
-            app.unique_markers.push(newmarker);
+            app.unique_markers1.push(newmarker);
             num_new_markers++;
         }
     }
     if(num_new_markers>0){
         console.log("adding "+num_new_markers+"new markers");
-        ShowMarkers(num_new_markers);
+        ShowMarkers1(num_new_markers);
     }
 }
-function ShowMarkers(num_new_markers){
+function FillUniqueMarkers2(data){
+    console.log(data.results);
+    var num_new_markers=0;
+    //var unique_markers = [];
+    for(var i=0; i < data.results.length; i++){
+        
+        //if there is no markers in the array, add the first
+        if(app.unique_markers2.length==0){
+            var newmarker = new unique_marker(data.results[i].coordinates.latitude, data.results[i].coordinates.longitude);
+            newmarker.addParameter(data.results[i].parameter,data.results[i].value);
+            app.unique_markers2.push(newmarker);
+            num_new_markers++;
+        }
+
+        //check that if the new marker already exists in the unique_markers
+        var exists = false;
+        for(var j = 0; j < app.unique_markers2.length; j++) {
+            if (app.unique_markers2[j].coordinates.latitude == data.results[i].coordinates.latitude && app.unique_markers2[j].coordinates.longitude == data.results[i].coordinates.longitude){
+                exists = true;
+                app.unique_markers2[j].addParameter(data.results[i].parameter,data.results[i].value);
+            }
+        }
+        if(!exists){
+            var newmarker = new unique_marker(data.results[i].coordinates.latitude, data.results[i].coordinates.longitude);
+            newmarker.addParameter(data.results[i].parameter,data.results[i].value);
+            app.unique_markers2.push(newmarker);
+            num_new_markers++;
+        }
+    }
+    if(num_new_markers>0){
+        console.log("adding "+num_new_markers+"new markers");
+        ShowMarkers2(num_new_markers);
+    }
+}
+function ShowMarkers1(num_new_markers){
+    var num_old_markers = app.map1_marker_objects.length;
+    for(var i = 0; i<num_new_markers; i++){
+       
+        var marker = addMarker([app.unique_markers1[i+num_old_markers].coordinates.latitude,app.unique_markers1[i+num_old_markers].coordinates.longitude], app.map1)
+            .bindPopup(GetPopupString(app.unique_markers1[i+num_old_markers]));
+        marker.on('mouseover', function (e) {
+            this.openPopup();
+        });
+        marker.on('mouseout', function (e) {
+            this.closePopup();
+        });
+        app.map1_marker_objects.push(marker);
+    }
+    console.log("map 1 currently has :"+app.unique_markers1.length + " markers");
+}
+function ShowMarkers2(num_new_markers){
     var num_old_markers = app.map2_marker_objects.length;
     for(var i = 0; i<num_new_markers; i++){
-        var marker = addMarker([app.unique_markers[i+num_old_markers].coordinates.latitude,app.unique_markers[i+num_old_markers].coordinates.longitude], app.map2)
-            .bindPopup(GetPopupString(app.unique_markers[i+num_old_markers]));
+        var marker = addMarker([app.unique_markers2[i+num_old_markers].coordinates.latitude,app.unique_markers2[i+num_old_markers].coordinates.longitude], app.map2)
+            .bindPopup(GetPopupString(app.unique_markers2[i+num_old_markers]));
         marker.on('mouseover', function (e) {
             this.openPopup();
         });
@@ -214,7 +317,7 @@ function ShowMarkers(num_new_markers){
         });
         app.map2_marker_objects.push(marker);
     }
-    console.log("currently have :"+app.unique_markers.length + " markers");
+    console.log("map 2 currently has :"+app.unique_markers2.length + " markers");
 }
 function GetPopupString(unique_marker){
     var retstring="";
@@ -273,17 +376,17 @@ addMarker = function([lat,lng],map){
 /*   Loops    */
 DeleteOldMarkers = function(){
     var num_deleted=0;//for printing purposes only
-    var i = app.unique_markers.length
+    var i = app.unique_markers2.length
     while (i--) {
-        if(!app.map2.getBounds().contains([app.unique_markers[i].coordinates.latitude,app.unique_markers[i].coordinates.longitude])){ 
-            app.unique_markers.splice(i, 1);
+        if(!app.map2.getBounds().contains([app.unique_markers2[i].coordinates.latitude,app.unique_markers2[i].coordinates.longitude])){ 
+            app.unique_markers2.splice(i, 1);
             app.map2.removeLayer(app.map2_marker_objects[i]);
             app.map2_marker_objects.splice(i,1);
             num_deleted++;
         } 
     }
     console.log("deleted :"+num_deleted + " markers");
-    console.log("currently have :"+app.unique_markers.length + " markers");
+    console.log("currently have :"+app.unique_markers2.length + " markers");
 }
 fullscreen1 = function(){
     document.getElementById("map2_and_nav").style.display="none";
@@ -322,4 +425,3 @@ halfscreen2 = function(){
     app.map2.invalidateSize();
     //DeleteOldMarkers();
 }
-
